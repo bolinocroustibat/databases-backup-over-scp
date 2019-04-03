166a3f5ce722383d7fde9de9d077775125cb9327
Adrien Carpentier <moi@adriencarpentier.com>
2019-04-03 17:42:17 +0200

    Clean example_settings comments
    Update example_settings.py

--- example_settings.py
+++ example_settings.py
@@ -1,17 +1,17 @@
-## PostgreSQL database details to which backup to be done.
-POSTGRES_DB_NAMES = ['db1_name','db2_name'] # PostgreSQL databases to backup
-POSTGRES_SYSTEM_USER = 'postgres' # make sure this user having enough privileges to take all databases backup.
+## PostgreSQL database details to which backup to be done. Used only by backup-postgresql.py script.
+POSTGRES_DB_NAMES = ['db1_name','db2_name'] # PostgreSQL databases names to backup.
+POSTGRES_SYSTEM_USER = 'postgres' # make sure this system user has enough privileges to take all databases backup.
 
-## MySQL database details to which backup to be done. Make sure below user having enough privileges to take databases backup.
+## MySQL database details to which backup to be done. Used only by backup-mysql.py script.
 MYSQL_DB_NAMES = ['db1_name','db2_name'] # MySQL databases to backup
-MYSQL_USER = ''
+MYSQL_USER = '' # make sure this MySQL user has enough privileges to take all databases backup.
 MYSQL_USER_PASSWORD = ''
 
 ## Local setting
-LOCAL_PATH = '/root/backup-sql/dumps/' # full local path where dumps will be saved, with trailing slash.
-LOGFILE = '/root/backup-sql/log-last-script.log' # full path to log file.
+LOCAL_PATH = '/root/databases-backup-script-over-scp/dumps/' # full local path where dumps will be saved, with trailing slash.
+LOGFILE = '/root/databases-backup-script-over-scp/log-last-script.log' # full path to log file.
 
 ### Remote settings
-REMOTE_URL = '' # leave blank if you don't want to save remotely
+REMOTE_URL = '' # leave blank if you don't want to save remotely.
 REMOTE_USER = '' # you need to be authorized on remote with your user SSH keys.
 REMOTE_PATH = '/home/backup-sql/' # full remote path where dumps will be saved.
