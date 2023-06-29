b104b60cf3459a3e075c90037b9fb1e54e1bdfa2
Adrien Carpentier <me@adriencarpentier.com>
2023-06-29 12:22:31 +0200

    fix: better exception management

--- backup-mysql.py
+++ backup-mysql.py
@@ -11,24 +11,25 @@ logger = FileLogger()
 # Create local backup folder
 try:
     os.makedirs(TODAY_LOCAL_PATH)
-    logger.log(f"Local backup folder {TODAY_LOCAL_PATH} created.")
 except Exception as e:
     logger.log(f"### ERROR ### while creating local backup folder: {str(e)}")
+else:
+    logger.log(f"Local backup folder {TODAY_LOCAL_PATH} created.")
 
-# Local backup
-for db in MYSQL_DB_NAMES:
-    try:
-        dump_cmd: str = f"mysqldump --user={MYSQL_USER} --password={MYSQL_USER_PASSWORD} {db} > {TODAY_LOCAL_PATH}/{db}.sql"  # noqa E501
-        os.system(dump_cmd)
-        logger.log(f"Backup dump file {db}.sql has been saved locally.")
-    except Exception as e:
-        logger.log(
-            f"### ERROR ### while trying to dump the database '{db}' locally: {str(e)}"
-        )
-
-# Remote backup
-if REMOTE_HOST:
-    remote_backup(db_names=MYSQL_DB_NAMES, logger=logger)
+    # Local backup
+    for db in MYSQL_DB_NAMES:
+        try:
+            dump_cmd: str = f"mysqldump --user={MYSQL_USER} --password={MYSQL_USER_PASSWORD} {db} > {TODAY_LOCAL_PATH}/{db}.sql"  # noqa E501
+            os.system(dump_cmd)
+        except Exception as e:
+            logger.log(
+                f"### ERROR ### while trying to dump the database '{db}' locally: {str(e)}"  # noqa E501
+            )
+        else:
+            logger.log(f"Backup dump file {db}.sql has been saved locally.")
+            # Remote backup
+            if REMOTE_HOST:
+                remote_backup(db_names=MYSQL_DB_NAMES, logger=logger)
 
 logger.log("MySQL backup script completed.")
 
--- backup-postgresql.py
+++ backup-postgresql.py
@@ -11,26 +11,25 @@ logger = FileLogger()
 # Create local backup folder
 try:
     os.system(f'su -c "mkdir -p {TODAY_LOCAL_PATH}" {POSTGRES_SYSTEM_USER}')
-    logger.log(f"Local backup folder {TODAY_LOCAL_PATH} created.")
 except Exception as e:
     logger.log(f"### ERROR ### while creating local backup folder: {str(e)}")
+else:
+    logger.log(f"Local backup folder {TODAY_LOCAL_PATH} created.")
 
-# Local backup
-for db in POSTGRES_DB_NAMES:
-    try:
-        dump_cmd: str = (
-            f'su -c "pg_dump {db} > {TODAY_LOCAL_PATH}/{db}.sql" {POSTGRES_SYSTEM_USER}'
-        )
-        os.system(dump_cmd)
-        logger.log(f"Backup dump file {db}.sql has been saved locally.")
-    except Exception as e:
-        logger.log(
-            f"### ERROR ### while trying to dump the database {db} locally: {str(e)}"
-        )
-
-# Remote backup
-if REMOTE_HOST:
-    remote_backup(db_names=POSTGRES_DB_NAMES, logger=logger)
+    # Local backup
+    for db in POSTGRES_DB_NAMES:
+        try:
+            dump_cmd: str = f'su -c "pg_dump {db} > {TODAY_LOCAL_PATH}/{db}.sql" {POSTGRES_SYSTEM_USER}'  # noqa E501
+            os.system(dump_cmd)
+        except Exception as e:
+            logger.log(
+                f"### ERROR ### while trying to dump the database {db} locally: {str(e)}"  # noqa E501
+            )
+        else:
+            logger.log(f"Backup dump file {db}.sql has been saved locally.")
+            # Remote backup
+            if REMOTE_HOST:
+                remote_backup(db_names=POSTGRES_DB_NAMES, logger=logger)
 
 logger.log("PostgreSQL backup script completed.")
 
