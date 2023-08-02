cbf3a7d1787eef7454f07d6ef60aa36a7ca938f1
Adrien Carpentier <me@adriencarpentier.com>
2023-08-02 11:08:56 +0200

    fix: remove second in folder path name

--- helpers/paths.py
+++ helpers/paths.py
@@ -3,6 +3,6 @@ from datetime import datetime
 from settings import LOCAL_PATH, REMOTE_PATH
 
 
-today: str = datetime.utcnow().strftime("%Y-%m-%d--%H-%M-%S")
+today: str = datetime.utcnow().strftime("%Y-%m-%d--%H-%M")
 TODAY_LOCAL_PATH = LOCAL_PATH + today
 TODAY_REMOTE_PATH = REMOTE_PATH + today
