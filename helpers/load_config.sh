b667a51d1f6c89343f0d8edc9c53b3e1fdf12885
Adrien Carpentier <me@adriencarpentier.com>
2024-10-02 14:03:32 +0900

    fix: safer load_config.sh

--- helpers/load_config.sh
+++ helpers/load_config.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
 config() {
-    local section=$1
-    local key=$2
-    local value_type=$(yq eval ".${section}.${key} | type" "config.yaml")
+    local section="$1"
+    local key="$2"
+
+    # Validate inputs
+    if [[ -z "$section" || -z "$key" ]]; then
+        echo "Usage: config <section> <key>"
+        return 1
+    fi
+
+    # Fetch the value and check its type
+    local value
+    value=$(yq eval ".${section}.${key}" "config.yaml" 2>/dev/null)
+
+    if [[ $? -ne 0 ]]; then
+        echo "Error: Could not read from config.yaml"
+        return 1
+    fi
+
+    local value_type
+    value_type=$(yq eval ".${section}.${key} | type" "config.yaml" 2>/dev/null)
 
     if [[ $value_type == "!!seq" ]]; then
         # Return the list items
-        yq eval ".${section}.${key} | .[]" "config.yaml"
+        echo "$value" | yq eval '.[]' - 
     else
         # Return the scalar value
-        yq eval ".${section}.${key}" "config.yaml"
+        echo "$value"
     fi
 }
